743e5a279b08dd3be4751ed88a468c3b3b8eb1f4
ever2after <dhwntkd412@snu.ac.kr>
2023-08-15 17:48:58 +0900

    Add vizwiz dataset

---
+++ lavis/datasets/datasets/vizwiz_datasets.py
@@ -0,0 +1,68 @@
+import os
+import pandas as pd
+from PIL import Image
+import torch
+
+from lavis.datasets.datasets.base_dataset import BaseDataset
+
+
+class VizWizDataset(BaseDataset):
+    def __init__(self, vis_processor, text_processor, vis_root, ann_paths):
+        super().__init__(vis_processor, text_processor, vis_root, ann_paths=[])
+        self.annotation = []
+        for ann in ann_paths:
+            # self.annotation.extend(pd.read_parquet(ann))
+            # self.annotation = pd.read_parquet(ann)
+            self.annotation = pd.read_json(ann)
+
+    def __getitem__(self, index):
+        ann = self.annotation.iloc[index]
+
+        image_path = os.path.join(self.vis_root, ann["image"])
+        image = Image.open(image_path).convert("RGB")
+
+        image = self.vis_processor(image)
+
+        instruction = f'<Image> Question: {ann["question"]} Short answer:'
+        
+        instruction = self.text_processor(instruction)
+
+        answer = ann["answers"][0]["answer"] # 0~9
+
+        return {
+            "image": image,
+            "text_input": instruction,
+            "text_output" : answer,
+        }
+    
+    
+class VizWizDataset(BaseDataset):
+    def __init__(self, vis_processor, text_processor, vis_root, ann_paths):
+        super().__init__(vis_processor, text_processor, vis_root, ann_paths=[])
+        self.annotation = []
+        for ann in ann_paths:
+            # self.annotation.extend(pd.read_parquet(ann))
+            # self.annotation = pd.read_parquet(ann)
+            self.annotation = pd.read_json(ann)
+
+    def __getitem__(self, index):
+        ann = self.annotation.iloc[index]
+
+        image_path = os.path.join(self.vis_root, ann["image"])
+        image = Image.open(image_path).convert("RGB")
+
+        image = self.vis_processor(image)
+
+        instruction = f'<Image> Question: {ann["question"]} Short answer:'
+        
+        instruction = self.text_processor(instruction)
+
+        answer = ann["answers"][0]["answer"] # 0~9
+
+        return {
+            "image": image,
+            "text_input": instruction,
+            "text_output" : answer,
+        }
+    
+    
