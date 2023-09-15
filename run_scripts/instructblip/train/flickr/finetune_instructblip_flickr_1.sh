2a169fdf688c33148af4be317fadc917bf1f6032
aschung01 <achung20@gmail.com>
2023-09-15 04:12:11 +0000

    ADD : Added NFS input exports

--- run_scripts/instructblip/train/flickr/finetune_instructblip_flickr_1.sh
+++ run_scripts/instructblip/train/flickr/finetune_instructblip_flickr_1.sh
@@ -1,6 +1,8 @@
 mkdir -p /output/results/flickr/flickr_1
 mkdir -p /input/results/flickr/flickr_1
 touch /output/results/flickr/flickr_1/flickr_1.log
+export TORCH_HOME=/input/torch
+export HUGGINGFACE_HUB_CACHE=/input/huggingface
 nohup python3 -m torch.distributed.run --nproc_per_node=1 train.py --cfg-path lavis/projects/instructblip/train/flickr/finetune_instructblip_flickr_1.yaml 2>&1 | tee /output/results/flickr/flickr_1/flickr_1.log
 wait
 touch /input/results/flickr/flickr_1/flickr_1.log
